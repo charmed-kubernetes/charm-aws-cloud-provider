e08d1c6854ad2cc6c2433a65dfd6ddb86dec9d07
Adam Dyess <adam.dyess@canonical.com>
2025-01-17 12:53:56 -0600

    Create Terrform modules for this charm

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+resource "juju_application" "aws_cloud_provider" {
+  name  = var.app_name
+  model = var.model
+
+  charm {
+    name     = "aws-cloud-provider"
+    channel  = var.channel
+    revision = var.revision
+    base     = var.base
+  }
+
+  config      = var.config
+  constraints = var.constraints
+}
